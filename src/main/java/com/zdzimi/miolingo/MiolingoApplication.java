3403bf97fd38b579a90aeabe77df603ebf76d7a7
Michal <michalzdziennickip@gmail.com>
2024-01-06 11:44:29 +0100

    Set up project

---
+++ src/main/java/com/zdzimi/miolingo/MiolingoApplication.java
@@ -0,0 +1,13 @@
+package com.zdzimi.miolingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiolingoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MiolingoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/zdzimi/miolingo/MiolingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdzimi.miolingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiolingoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
