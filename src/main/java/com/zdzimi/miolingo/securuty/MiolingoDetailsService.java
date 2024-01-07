1947512bd5fef2eaaa1e945289943dadb0451197
Michal <michalzdziennickip@gmail.com>
2024-01-07 15:52:52 +0100

    Set up security configuration

---
+++ src/main/java/com/zdzimi/miolingo/securuty/MiolingoDetailsService.java
@@ -0,0 +1,23 @@
+package com.zdzimi.miolingo.securuty;
+
+import com.zdzimi.miolingo.data.model.UserEntity;
+import com.zdzimi.miolingo.data.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MiolingoDetailsService implements UserDetailsService {
+
+  private final UsersRepository usersRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    return new PrincipalUser(usersRepository.findById(email)
+        .orElseThrow(() -> new UsernameNotFoundException(String.format("404 User %s not found.", email))));
+  }
+
+}
---
+++ src/main/java/com/zdzimi/miolingo/securuty/WebSecurityConfiguration.java
@@ -0,0 +1,58 @@
+package com.zdzimi.miolingo.securuty;
+
+import java.util.Arrays;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfiguration {
+
+  private final UserDetailsService userDetailsService;
+  private final PasswordEncoder passwordEncoder;
+
+  @Bean
+  public DaoAuthenticationProvider daoAuthenticationProvider() {
+    DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    authenticationProvider.setUserDetailsService(userDetailsService);
+    authenticationProvider.setPasswordEncoder(passwordEncoder);
+    return authenticationProvider;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+        req -> req
+            .requestMatchers("/miolingo/sign-up").permitAll()
+            .anyRequest().authenticated()
+    ).formLogin(Customizer.withDefaults());
+    return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
+    configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+    configuration.setAllowCredentials(true);
+    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+
+}
+
